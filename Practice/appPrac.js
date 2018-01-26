3b5b9282677a5cf68c53e987653443fc2acaacd4
Ariel Fonticiella <ariel@fonticiella.io>
2018-01-26 15:04:31 -0500

    set up some IIFEs

--- Practice/appPrac.js
+++ Practice/appPrac.js
@@ -26,3 +26,38 @@ logGreeting(greetMe);
 logGreeting(function(){
     console.log('Hello Ariel!');
 });
+
+//========================================================================
+
+// pass "by Value"
+function change(b) {
+    b = 2;
+}
+
+var a = 1;
+change(a);
+console.log(a);
+
+// pass by reference
+function changeObj(d) {
+    d.prop1 = function() {};
+    d.prop2 = {};
+}
+
+var c = {};
+c.prop1 = {};
+changeObj(c);
+
+//========================================================================
+
+// Immediately Invoked Function Expression (IIFEE)
+
+(function () {
+
+    var firstname = 'John';
+    console.log(firstname);
+
+}());
+
+var firstname = 'Jane';
+console.log(firstname);
